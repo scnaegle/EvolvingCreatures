88ffb20cd227face8242390d40df6ef9b5085e94
Julian Weisburd <jmweisburd@unm.edu>
2015-10-16 17:25:24 -0600

    Added more comments

--- src/vcreature/creatureUtil/RandomCreature.java
+++ src/vcreature/creatureUtil/RandomCreature.java
@@ -11,11 +11,17 @@ import java.util.Random;
 /**
  * Class responsible for random creature generation
  * Creature generation begins with a root node and follows set of rules to create random valid creature
+ * Extends Joel's Creature class
  */
 public class RandomCreature extends Creature
 {
   private static Random rand = new Random();
 
+  /**
+   * Default constructor, attaches creature to world
+   * @param physicsSpace
+   * @param jMonkeyRootNode
+   */
   public RandomCreature(PhysicsSpace physicsSpace, Node jMonkeyRootNode)
   {
     super(physicsSpace, jMonkeyRootNode);
@@ -32,10 +38,12 @@ public class RandomCreature extends Creature
     Vector3f rootCenter = new Vector3f(0f,0f,0f);
     Vector3f rootSize = new Vector3f();
 
+    //choose random x,y,z values between 1 and 10
     rootSize.x = ((rand.nextInt(9)+1) + rand.nextFloat())/2;
     rootSize.y = ((rand.nextInt(9)+1) + rand.nextFloat())/2;
     rootSize.z = ((rand.nextInt(9)+1) + rand.nextFloat())/2;
 
+    //bump up the root node so it doesn't overlap with floor
     rootCenter.y = rootSize.y;
 
     return addRoot(rootCenter, rootSize);
