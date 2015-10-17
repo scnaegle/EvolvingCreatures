181184ed06d25b29b65e4ce13fcfb7b93a3fe096
Julian Weisburd <jmweisburd@unm.edu>
2015-10-17 13:12:15 -0600

    Added comments to code

--- src/vcreature/creatureUtil/RandomCreature.java
+++ src/vcreature/creatureUtil/RandomCreature.java
@@ -8,6 +8,19 @@ import vcreature.phenotype.Creature;
 
 import java.util.Random;
 
+// Only 10 max blocks, generate number from 0-9
+// while blocklist.size less than block number
+//
+// Choose random block from blocklist to add too
+// Choose random side from chosen block to add too
+// Choose random get appropriate side from newly created block
+// Choose random point on block surface to add to
+// Choose random point on edge of newly created block
+// Attach block: make sure no overlap
+//
+// Give neurons to blocks joints
+//
+
 /**
  * Class responsible for random creature generation
  * Creature generation begins with a root node and follows set of rules to create random valid creature
@@ -26,7 +39,15 @@ public class RandomCreature extends Creature
   {
     super(physicsSpace, jMonkeyRootNode);
 
+    int blockNumber = rand.nextInt(9)+1;
+
     Block root = makeRoot();
+
+    while (this.getNumberOfBodyBlocks() < blockNumber)
+    {
+      //int parentBlock = rand.nextInt(getNumberOfBodyBlocks) + 1;
+      //addRandomBlock(parentBlock);
+    }
 
   }
 
@@ -49,6 +70,16 @@ public class RandomCreature extends Creature
     return addRoot(rootCenter, rootSize);
   }
 
+  private void addRandomBlock(int index)
+  {
+    //Block parent = this.getBlockByID(index);
+    //Block child = newRandomBlock();
+  }
+
+  //private Block newRandomBlock()
+  //{
+    Vector3f rootSize = new Vector3f();
+  //}
 
 
 
